d43be0712951b200c972fb3e3fa408c0bee8e875
Mohamed Said <mohamedsaidbenyaiche@gmail.com>
2026-02-07 21:51:31 +0100

    Fix

--- routes/web.php
+++ routes/web.php
@@ -3,16 +3,4 @@
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\EvenementController;
 
-/*
-|--------------------------------------------------------------------------
-| Home page = Event list
-|--------------------------------------------------------------------------
-*/
-Route::get('/', [EvenementController::class, 'index'])->name('home');
-
-/*
-|--------------------------------------------------------------------------
-| Resource routes
-|--------------------------------------------------------------------------
-*/
 Route::resource('evenements', EvenementController::class)->except(['index']);
