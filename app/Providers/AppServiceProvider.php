9eda38f9b15d07a415dd7c40ae90a56be4635b30
Mohamed Said <mohamedsaidbenyaiche@gmail.com>
2026-02-07 22:00:29 +0100

    Fix

--- app/Providers/AppServiceProvider.php
+++ app/Providers/AppServiceProvider.php
@@ -3,7 +3,6 @@
 namespace App\Providers;
 
 use Illuminate\Support\ServiceProvider;
-use Illuminate\Support\Facades\URL; // 1. Important: Import the URL facade
 
 class AppServiceProvider extends ServiceProvider
 {
@@ -20,11 +19,6 @@ class AppServiceProvider extends ServiceProvider
      */
     public function boot(): void
     {
-        /** * 2. Force HTTPS for all generated URLs (CSS, JS, Forms) 
-         * only when the application is running in production.
-         */
-        if (config('app.env') === 'production') {
-            URL::forceScheme('https');
-        }
+        //
     }
 }
